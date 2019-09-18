b38b2804fdf5ba619069483d6de04e0ee40cda86
gajjuCoderBoi <40663087+gajjuCoderBoi@users.noreply.github.com>
2019-09-18 17:03:17 -0400

    Create fizzbuzz.js
    Fizzbuzz HW -  Done

---
+++ fizzbuzz.js
@@ -0,0 +1,6 @@
+for (let i=1;i<=101;i++){
+    if (!(i%3) && !(i%5)) console.log("fizzbuzz");
+    if (!(i%3)) console.log("fizz");
+    else if(!(i%5)) console.log("buzz");
+    else console.log(i);
+}
